65b5045b074b3d93da84a975805c0d5f5521dbeb
yasasvi1312 <yasasvi6thadiboyina@gmail.com>
2024-02-04 16:56:38 +0530

    Add project files.

---
+++ ePizzaHub.Repositories/Implementation/Repository.cs
@@ -0,0 +1,47 @@
+﻿using ePizzaHub.Core;
+using ePizzaHub.Repositories.Interfaces;
+
+namespace ePizzaHub.Repositories.Implementation
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        AppDbContext _db;
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public void Add(TEntity entity)
+        {
+            _db.Set<TEntity>().Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            TEntity entity = _db.Set<TEntity>().Find(id);
+            if (entity != null)
+            {
+                _db.Set<TEntity>().Remove(entity);
+            }
+        }
+
+        public TEntity Find(object id)
+        {
+            return _db.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _db.Set<TEntity>().ToList();
+        }
+
+        public int SaveChanges()
+        {
+            return _db.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _db.Set<TEntity>().Update(entity);
+        }
+    }
+}
